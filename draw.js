8ff0d4caa00ca3866b5175e8e952d7d58611d163
Sonny65 <sonnyliu65@gmail.com>
2018-03-09 11:14:27 -0800

    colored_border

--- draw.js
+++ draw.js
@@ -1,27 +1,37 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function draw(tree,x1,y1){
 	var coordinate;
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
-	ctx.lineWidth="3";
+	ctx.lineWidth="4";
 	if(tree.hasOwnProperty("children")){
 		x2 = x1+50;
 		y2 = y1+50;
-		maxY =
 		for(var i = 0; i < tree.children.length; i++){
 			coordinate = draw(tree.children[i],x2,y1+50);
 			x2 = coordinate[0]+50;
 			if (coordinate[1]+50 > y2) {
-				console.log(y2+"before");
 				y2 = coordinate[1]+50;
-				console.log(y2);
 			}
 		}
+		ctx.beginPath();
 		ctx.rect(x1, y1, x2-x1, y2-y1);
+		ctx.strokeStyle=getRandomColor();
 		ctx.stroke();
 		return [x2,y2];
 	} else {
 		x2 = x1 + 50;
+		ctx.beginPath();
 		ctx.rect(x1, y1, 50, 50);
+		console.log(ctx.strokeStyle=getRandomColor());
 		ctx.stroke();
 		return [x2,y1 + 50];
 	}
