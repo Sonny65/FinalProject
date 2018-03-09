442c972e964b22b3a35ad382959271d0a13ad919
URDeVotee <Sonny@campus-105-189.ucdavis.edu>
2018-03-08 16:06:22 -0800

    draw.js added

--- script.js
+++ script.js
@@ -105,30 +105,3 @@ function sortSets(){
 function ComEDRecursive(){
 	
 }
-
-function drawComED(){
-	testhierarchy = [ 	{"set": 0, "elements": [4], 
-							"children": [ 
-								{"set": 1, "elements": [5], 
-									"children": [ 
-										{"set": 3, "elements": [6]}
-									] 
-								},
-								{"set": 2, "elements": [2], 
-									"children": [
-										{"set": 3, "elements": [1]}
-									]
-								}
-							]
-					  	},
-					  	{"set": 1, "elements": [8], 
-					  		"children": [
-					  			{"set": 2, "elements": [3]}
-					  		]
-					  	},
-					  	{"set": 2, "elements": [0]},
-					  	{"set": 3, "elements": [7]}
-					];
-
-
-}
