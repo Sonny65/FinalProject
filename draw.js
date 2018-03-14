2b9cc658d79c786851ae5862aa3c396faa4af82f
URDeVotee <Sonny@campus-016-102.ucdavis.edu>
2018-03-13 18:01:49 -0700

    txt and element update

--- draw.js
+++ draw.js
@@ -1,5 +1,5 @@
 function getRandomColor(number) {
-  var letters = ["green","red","orange","yellow","pink","purple"];
+  var letters = ["green","red","orange","yellow","pink","white"];
   color = letters[number];
   return color;
 }
@@ -24,41 +24,66 @@ function fillRoundedRect(x, y, w, h, r, color){
   	ctx.stroke();
 }
 
+function fillRoundedRectEle(x, y, w, h, r, color,name){
+  	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.lineWidth="4";
+  	ctx.beginPath();
+  	ctx.moveTo(x+r, y);
+  	ctx.lineTo(x+w-r, y);
+  	ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+  	ctx.lineTo(x+w, y+h-r);
+  	ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+  	ctx.lineTo(x+r, y+h);
+  	ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+  	ctx.lineTo(x, y+r);
+  	ctx.quadraticCurveTo(x, y, x+r, y);
+  	ctx.strokeStyle = "black";
+  	ctx.fillStyle = getRandomColor(color);
+  	ctx.fill();
+  	ctx.stroke();
+  	ctx.fillStyle = "black";
+  	ctx.font = "30px Comic Sans MS";
+  	ctx.fillText(name,x+18,y+35);
+}
+
 function paint(dic){
   for (var key in dic){
     for (var set in dic[key]){
-      fillRoundedRect(dic[key][set][0], dic[key][set][1], dic[key][set][2], dic[key][set][3], 25, key);
+    	if(dic[key][set].length == 4){
+    		fillRoundedRect(dic[key][set][0], dic[key][set][1], dic[key][set][2], dic[key][set][3], 10, key);
+    	} else {
+    		fillRoundedRectEle(dic[key][set][0], dic[key][set][1], dic[key][set][2], dic[key][set][3], 10, key, dic[key][set][4])
+    	}
     }
   }
 }
 
 function design(tree,x1,y1,dic){
 	var coordinate;
-	if(tree.children.length > 0){
-		var x2 = x1+50;
-		var y2 = y1+50;
-		for(var i = 0; i < tree.children.length; i++){
-			coordinate = design(tree.children[i],x2,y1+50,dic);
-			x2 = coordinate[0]+50;
-			if (coordinate[1]+50 > y2) {
-				y2 = coordinate[1]+50;
-			}
-		}
-    dic[tree.set].push([x1, y1, x2-x1, y2-y1]);
-    // fillRoundedRect(x1, y1, x2-x1, y2-y1, 25, tree.set);
-		return [x2,y2];
-	} else {
-		x2 = x1 + 50;
-    dic[tree.set].push([x1, y1, 50, 50]);
-    // fillRoundedRect(x1, y1, 50, 50, 25, tree.set);
-		return [x2,y1 + 50];
+	var x2 = x1+10;
+	var y2 = y1+70;
+	for (var i = 0; i < tree.elements.length; i++){
+		dic[5].push([x2, y1+10, 50, 50, tree.elements[i]]);
+		x2 = x2 + 60;
 	}
+	for(var i = 0; i < tree.children.length; i++){
+		coordinate = design(tree.children[i],x2,y1+10,dic);
+		x2 = coordinate[0]+10;
+		if (coordinate[1]+10 > y2) {
+			y2 = coordinate[1]+10;
+		}
+	}
+	dic[tree.set].push([x1, y1, x2-x1, y2-y1]);
+	console.log(y2-y1);
+	// fillRoundedRect(x1, y1, x2-x1, y2-y1, 25, tree.set);
+	return [x2,y2];
 }
 
 function drawComED(hierarchy){
 	testhierarchy = [ 	{"set": 0, "elements": [4],
 							"children": [
-								{"set": 1, "elements": [5],
+								{"set": 1, "elements": [5,12,4,5],
 									"children": [
 										{"set": 2, "elements": [6], "children": []},
 										{"set": 2, "elements": [6],
